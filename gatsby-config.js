e2991e42f6da2f0f9b9c3ee947438d82d9a6e42a
Li Chen <chen1649chenli@gmail.com>
2019-04-21 15:41:01 -0500

    updated gatsby-config.js with my name

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'LiChen',
+        short_name: 'LiChen',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
